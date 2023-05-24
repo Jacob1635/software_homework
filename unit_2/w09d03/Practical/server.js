3f1a72ed46cee2173fbde3de18b7ac190ef1dcb2
Jacobzag <zagorenko1249@gmail.com>
2023-05-24 06:55:21 -0700

    Hello insert *name*

--- unit_2/w09d03/Practical/server.js
+++ unit_2/w09d03/Practical/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { get } = require('http');
 const app = express();
 
-app.get('/greeting', (req, res) => {
-    const greeting = 'Hello, stranger!';
-    res.send(greeting);
-  });
-  
-  app.listen(3000, () => {
+app.get('/greeting/:name', (req, res) => {
+        const name = req.params.name;
+        const greeting = `Hello, ${name}!`;
+        res.send(greeting);
+    });
+
+
+app.listen(3000, () => {
     console.log('Server listening on port 3000');
-  });
+});
